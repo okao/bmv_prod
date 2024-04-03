1ed5895a70107b8655dbe000228d2d9dcfafaf2a
okao <hamzathanees@gmail.com>
2024-04-04 04:23:05 +0500

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minoos-space.sgp1.cdn',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minoos-space.sgp1.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minoos-space.sgp1.cdn.digitaloceanspaces.com',
+      },
+    ],
+    unoptimized: true, // Cache images false and true means no cache
+    // loader: "cloudinary",
+    // path: "https://res.cloudinary.com/your-unique-account-id/",
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: ['src'],
+  },
+};
 
 export default nextConfig;
