dc5c7ec01573ceef482577b57e3e238bfea261bf
okao <hamzathanees@gmail.com>
2024-10-24 23:38:28 +0500

    Add Google Ads 14

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"18d39960840cfcb81ad8737d5dac8e87e2e99425\":{\"workers\":{\"app/(landing)/article/[id]/page\":8784},\"layer\":{\"app/(landing)/article/[id]/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"ruDbawOI8szqeTJYUOKqebYQn8Lvu66TYOHOSFJPbRg=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"18d39960840cfcb81ad8737d5dac8e87e2e99425\":{\"workers\":{\"app/(landing)/article/[id]/page\":8784},\"layer\":{\"app/(landing)/article/[id]/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"OfHWmbFTw+6KTX+kau0G2LeGdrDVNeFwla4yGUbCG5U=\"}"
